5c5bcc97f395fe324d221ca0373d0a9b23df0e2f
David Podhola <david@podhola.net>
2016-03-04 20:27:03 +0100

    in UITest run firefox and open todomvc (http://localhost:8090/sample/browser/todomvc/) see https://github.com/fsprojects/Fable/blob/master/docs/compiling.md#browser-samples
    run UITest in test_script

--- test_script.cmd
+++ test_script.cmd
@@ -15,5 +15,6 @@ call npm install
 cd ..\..\..
 node tools/fable2babel.js --projFile sample/browser/todomvc/app.fsx
 for /F %%A in ('powershell -Command "(Start-Process -PassThru -FilePath 'node' -ArgumentList 'sample/node/server/app.js 8090').Id"') do set PID=%%A
+build\uitest\Fable.UITests.exe
 timeout /t 10
 taskkill /PID %PID%  
--- uitest/Fable.UITests/Program.fs
+++ uitest/Fable.UITests/Program.fs
@@ -1,7 +1,20 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open canopy
+open runner
+open System
+
+"opening todo and entering one" &&& fun _ ->
+  url "http://localhost:8090/sample/browser/todomvc/"
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
-    0 // return an integer exit code
+
+    // firefox is reported to work very well on AppVeyor (phantomJS not)
+    start firefox
+
+    //run all tests
+    run()
+
+    quit()
+
+    0 
